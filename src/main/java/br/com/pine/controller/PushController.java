632a46a9d91fae510507f8d3702395ad2f8d4a6b
andrews <andrews.campos@hotmail.com>
2017-11-03 15:04:43 -0200

    ajuste

--- src/main/java/br/com/pine/controller/PushController.java
+++ src/main/java/br/com/pine/controller/PushController.java
@@ -1,13 +1,11 @@
 package br.com.pine.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import br.com.pine.bean.Push;
+import com.google.gson.Gson;
 
 @Controller
 public class PushController {
@@ -17,22 +15,22 @@ public class PushController {
 	public String index() {
 		return "index";
 	}
-	@RequestMapping(value = "/index", method=RequestMethod.GET)
-	public String indexs() {
-		return "index";
+		
+	@RequestMapping(value="/activity/save",method=RequestMethod.POST)
+	public void save(@RequestBody Object object) {
+		System.out.println(new Gson().toJson(object));
+	}
+	@RequestMapping(value="/activity/publish",method=RequestMethod.POST)
+	public void publish(@RequestBody Object object) {
+		System.out.println(new Gson().toJson(object));
+	}
+	@RequestMapping(value="/activity/validate",method=RequestMethod.POST)
+	public void validate(@RequestBody Object object) {
+		System.out.println(new Gson().toJson(object));
+	}
+	@RequestMapping(value="/activity/execute",method=RequestMethod.POST)
+	public void execute(@RequestBody Object object) {
+		System.out.println(new Gson().toJson(object));
 	}
 	
-	@RequestMapping(value = "/error", method=RequestMethod.GET)
-	public String error() {
-		return "index";
-	}
-	@RequestMapping(value = "/error", method=RequestMethod.POST)
-	public String error1() {
-		return "index";
-	}
-	
-	@RequestMapping(value="/sendPush",method=RequestMethod.POST)
-	public void sendPush(@RequestBody @Valid Push push) {
-		System.out.println(push);
-	}
 }
