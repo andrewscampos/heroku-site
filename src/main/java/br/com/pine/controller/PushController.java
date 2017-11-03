f01c6a411eed36c86a1d4834f55197d83637f301
andrews <andrews.campos@hotmail.com>
2017-11-03 11:22:06 -0200

    first commit

---
+++ src/main/java/br/com/pine/controller/PushController.java
@@ -0,0 +1,25 @@
+package br.com.pine.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.pine.bean.Push;
+
+@Controller
+public class PushController {
+
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/sendPush",method=RequestMethod.POST)
+	public void sendPush(@RequestBody @Valid Push push) {
+		System.out.println(push);
+	}
+}
