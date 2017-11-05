86fa1f7b448dddb92460e4ddc94023ac8f576bc9
andrews <andrews.campos@hotmail.com>
2017-11-04 23:33:55 -0200

    ajuste

---
+++ src/main/java/br/com/pine/controller/LoginController.java
@@ -0,0 +1,20 @@
+package br.com.pine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+	
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+	@RequestMapping(value = "/index", method=RequestMethod.GET)
+	public String index1() {
+		return "index";
+	}
+	
+}
